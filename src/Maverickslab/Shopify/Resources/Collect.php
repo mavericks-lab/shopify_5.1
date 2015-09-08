f0c13ba44703a318863baad24d9cff334288795a
jfunu <jfunu79@gmail.com>
2015-09-08 09:35:23 +0000

    added the collect resource

---
+++ src/Maverickslab/Shopify/Resources/Collect.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 3/17/15
+ * Time: 12:31 AM
+ */
+
+namespace Maverickslab\Shopify\Resources;
+
+
+use Maverickslab\Shopify\ApiRequestor;
+use Maverickslab\Shopify\Exceptions\ShopifyException;
+
+class Collect extends BaseResource{
+
+
+    public function __construct( ApiRequestor $requester){
+        $this->requester = $requester;
+        $this->requester->resource = '/admin/collects';
+    }
+}
