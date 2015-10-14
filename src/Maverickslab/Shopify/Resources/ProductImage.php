be06e857545cec94d7876b6a2fcb4c4eba9ee675
jfunu <jfunu79@gmail.com>
2015-10-14 01:44:30 +0000

    added product image resource

---
+++ src/Maverickslab/Shopify/Resources/ProductImage.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 3/17/15
+ * Time: 12:31 AM
+ */
+
+namespace Maverickslab\Shopify\Resources;
+
+
+use Maverickslab\Shopify\ApiRequestor;
+use Maverickslab\Shopify\Exceptions\ShopifyException;
+
+class Product extends BaseResource{
+
+
+    public function __construct( ApiRequestor $requester){
+        $this->requester = $requester;
+        $this->requester->resource = '/admin/products';
+    }
+
+    public function remove ( $id, $product_id = null )
+    {
+        if(is_null($product_id))
+            throw new ShopifyException('Product Id not provided');
+
+        if(is_null($id))
+            throw new ShopifyException('Product Image Id not provided');
+
+        $this->requester->resource = $this->requester->resource.'/'.$product_id.'/images/'.$id;
+
+        return $this->requester->delete( $id );
+    }
+}
